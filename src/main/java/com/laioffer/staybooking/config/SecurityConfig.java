b91b98078d18c63df62028bdf407396bc3f26568
Huihui Wu <huihwu@gmail.com>
2023-06-23 12:45:02 -0700

    add ElasticSearch

--- src/main/java/com/laioffer/staybooking/config/SecurityConfig.java
+++ src/main/java/com/laioffer/staybooking/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.laioffer.staybooking.config;
 
+import com.laioffer.staybooking.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,8 +8,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -16,23 +19,42 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //这里是authoritication过程
     private final DataSource dataSource;
+    private final JwtFilter jwtFilter;
 
-    public SecurityConfig(DataSource dataSource) {
+    public SecurityConfig(DataSource dataSource, JwtFilter jwtFilter) {
+
         this.dataSource = dataSource;
+        this.jwtFilter = jwtFilter;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/register/*").permitAll()
+//                .antMatchers(HttpMethod.POST,"/authenticate/*").permitAll()
+//                .antMatchers("/stays").permitAll()
+//                .antMatchers("/stays/*").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .csrf()
+//                .disable();
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/register/*").permitAll()
                 .antMatchers(HttpMethod.POST,"/authenticate/*").permitAll()
-                .antMatchers("/stays").permitAll()
-                .antMatchers("/stays/*").permitAll()
+                .antMatchers("/stays").hasAuthority("ROLE_HOST")
+                .antMatchers("/stays/*").hasAuthority("ROLE_HOST")
                 .anyRequest().authenticated()
                 .and()
                 .csrf()
                 .disable();
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
 
     @Bean
